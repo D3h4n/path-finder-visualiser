2d22632d1867a232c8b0ad75a4ba399c48ce7046
D3h4n <75465982+D3h4n@users.noreply.github.com>
2022-01-05 14:21:49 -0400

    Added path finding algo and showing path

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,199 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import Square from './Square';
+import Square, { Node, NodeType } from './Square';
 import './App.css';
+import Heap from 'heap-js';
 
-const SIZE = 15;
+export const SIZE = 10;
+
+enum Operation {
+  SelectStart,
+  SelectEnd,
+  SelectWalls,
+  FindPath,
+  PathFound,
+  PathNotFound,
+}
+
+interface State {
+  operation: number;
+  start?: string;
+  end?: [number, number];
+  diagonals: boolean;
+}
 
 function App() {
-  const [squares, setSquares] = useState<Map<number, number>>(new Map());
-  const [state, setState] = useState(0);
-  const [start, setStart] = useState(-1);
-  const [end, setEnd] = useState(-1);
+  const [squares, setSquares] = useState<Map<string, Node>>(new Map());
+  const [state, setState] = useState<State>({
+    operation: Operation.SelectStart,
+    diagonals: false,
+  });
 
   const reset = () => {
-    setSquares(new Map([...new Array(SIZE ** 2)].map((_, idx) => [idx, 0])));
-    setState(0);
+    setSquares(
+      new Map(
+        [...new Array(SIZE ** 2)].map((_, idx) => {
+          const x = idx % SIZE;
+          const y = Math.floor(idx / SIZE);
+
+          return [`${x},${y}`, new Node(x, y)];
+        })
+      )
+    );
+
+    setState((prev) => ({
+      ...prev,
+      operation: Operation.SelectStart,
+    }));
   };
 
   const getHeader = useCallback(() => {
-    switch (state) {
-      case 0:
+    switch (state.operation) {
+      case Operation.SelectStart:
         return 'Select start position';
 
-      case 1:
+      case Operation.SelectEnd:
         return 'Select end position';
 
-      case 2:
+      case Operation.SelectWalls:
         return 'Draw walls';
+
+      case Operation.FindPath:
+        return 'Calculating Path';
+
+      case Operation.PathFound:
+        return 'Path Found';
+
+      case Operation.PathNotFound:
+        return 'No Possible Path';
     }
   }, [state]);
 
   const handleUpdateSquare = useCallback(
-    ([x, y]) => {
+    (coord: string) => {
       setSquares((_prev) => {
-        const pos = y * SIZE + x;
+        let node: Node | undefined;
         const prev = new Map(_prev);
 
-        switch (state) {
-          case 0:
-            prev.set(pos, 1);
-            setState(1);
-            setStart(pos);
+        switch (state.operation) {
+          case Operation.SelectStart:
+            node = prev.get(coord);
+            if (!node) {
+              alert('error selecting start node');
+              break;
+            }
+
+            node.type = NodeType.Start;
+            node.gCost = 0;
+            node.calculateFCost();
+            setState((prev) => ({
+              ...prev,
+              start: coord,
+              operation: Operation.SelectEnd,
+            }));
             break;
 
-          case 1:
-            prev.set(pos, 2);
-            setState(2);
-            setEnd(pos);
+          case Operation.SelectEnd:
+            if (coord === state.start) break;
+            node = prev.get(coord.toString());
+
+            if (!node) {
+              alert('error selecting end node');
+              break;
+            }
+
+            node.type = NodeType.End;
+
+            setState((prev) => ({
+              ...prev,
+              operation: Operation.SelectWalls,
+              end: coord.split(',').map((x) => Number(x)) as [number, number],
+            }));
+
+            let [x, y] = coord.split(',').map((x) => Number(x));
+            for (let node of squares.values()) {
+              node.calculateHCost([x, y]);
+            }
             break;
 
-          case 2:
-            if (pos === start || pos === end) break;
-            prev.set(pos, 3);
+          case Operation.SelectWalls:
+            if (coord === state.start || coord === state.end?.toString()) break;
+            node = prev.get(coord.toString());
+
+            if (!node) {
+              alert('error selecting wall');
+              break;
+            }
+
+            node.type = NodeType.Wall;
+
             break;
         }
 
         return prev;
       });
     },
-    [setSquares, state, end, start]
+    [state, squares]
   );
 
-  const handleStart = () => {
-    console.log(`Start: ${start}, End: ${end}`);
-  };
+  const handleStart = useCallback(() => {
+    setState((prev) => ({ ...prev, operation: Operation.FindPath }));
+
+    setSquares((_prev) => {
+      const prev = new Map(_prev);
+
+      const openList = new Heap<Node>((a, b) => a.fCost - b.fCost);
+      const closedList = new Set<string>();
+      const visited = new Set<string>();
+
+      openList.add(prev.get(state.start!)!);
+
+      while (openList.size() > 0) {
+        const node = openList.pop()!;
+        node.type = NodeType.Visited;
+        closedList.add(node.coord);
+        visited.add(node.coord);
+
+        for (let [coord, cost] of node.neighbours) {
+          const neighbour = prev.get(coord)!;
+
+          if (!state.diagonals && cost === 1.4) continue;
+
+          if (closedList.has(neighbour.coord)) continue;
+          if (neighbour.type === NodeType.Wall) continue;
+          if (neighbour.coord === state.end?.toString()) {
+            let pathNode: Node | undefined = node;
+
+            while (pathNode) {
+              pathNode.type = NodeType.Path;
+              pathNode = pathNode.prevNode;
+            }
+
+            prev.get(state.start!)!.type = NodeType.Start;
+            setState((prev) => ({ ...prev, operation: Operation.PathFound }));
+            return prev;
+          }
+
+          let newCost = node.gCost + cost;
+
+          if (newCost < neighbour.gCost) {
+            neighbour.gCost = newCost;
+            neighbour.calculateFCost();
+            neighbour.prevNode = node;
+          }
+
+          if (!visited.has(coord)) {
+            neighbour.type = NodeType.Searching;
+            visited.add(coord);
+            openList.add(neighbour);
+          }
+        }
+      }
+
+      setState((prev) => ({ ...prev, operation: Operation.PathNotFound }));
+      prev.get(state.start!)!.type = NodeType.Start;
+      return prev;
+    });
+  }, [state]);
 
   useEffect(reset, []);
   return (
@@ -75,11 +207,10 @@ function App() {
           alignSelf: 'center',
         }}
       >
-        {[...squares.entries()].map(([pos, value]) => (
+        {[...squares.entries()].map(([_, node], idx) => (
           <Square
-            key={pos}
-            pos={[pos % SIZE, Math.floor(pos / SIZE)]}
-            value={value}
+            key={idx}
+            node={node}
             handleUpdateSquare={handleUpdateSquare}
           />
         ))}
@@ -91,15 +222,26 @@ function App() {
           right: '2rem',
           display: 'flex',
           justifyContent: 'space-between',
+          flexDirection: 'column',
         }}
       >
         <button onClick={() => reset()}>Reset</button>
         <button
-          style={{ display: state < 2 ? 'none' : 'block' }}
+          disabled={state.operation !== Operation.SelectWalls}
           onClick={() => handleStart()}
         >
           Start
         </button>
+        <div>
+          <label>Diagonals</label>
+          <input
+            type="checkbox"
+            checked={state.diagonals}
+            onChange={(e) =>
+              setState((prev) => ({ ...prev, diagonals: e.target.checked }))
+            }
+          />
+        </div>
       </div>
     </>
   );
--- src/Square.tsx
+++ src/Square.tsx
@@ -1,43 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { SIZE } from './App';
 
-interface Props {
-  pos: [number, number];
-  value: number;
-  handleUpdateSquare: (pos: [number, number]) => void;
+export enum NodeType {
+  Empty,
+  Wall,
+  Start,
+  End,
+  Path,
+  Searching,
+  Visited,
 }
 
-export default function Square({ pos, value, handleUpdateSquare }: Props) {
+const NodeColor = new Map([
+  [NodeType.Empty, 'white'],
+  [NodeType.Wall, '#222'],
+  [NodeType.Path, 'gray'],
+  [NodeType.End, 'blue'],
+  [NodeType.Start, 'green'],
+  [NodeType.Searching, 'lightblue'],
+  [NodeType.Visited, 'red'],
+]);
+interface Props {
+  node: Node;
+  handleUpdateSquare: (pos: string) => void;
+}
+
+export default function Square({ node, handleUpdateSquare }: Props) {
   const [backgroundColor, setBackgroundColor] = useState<string>();
 
   const handleUpdateBackground = () => {
-    let color = '';
-
-    switch (value) {
-      case 1:
-        color = 'red';
-        break;
-
-      case 2:
-        color = 'red';
-        break;
-
-      case 3:
-        color = 'gray';
-        break;
-
-      default:
-        color = 'white';
-        break;
-    }
-
-    setBackgroundColor(color);
+    setBackgroundColor(NodeColor.get(node.type));
   };
 
-  useEffect(handleUpdateBackground, [value]);
+  useEffect(handleUpdateBackground, [node.type]);
 
   return (
     <div
       style={{
+        position: 'relative',
         height: `2rem`,
         width: `2rem`,
         backgroundColor,
@@ -47,10 +47,103 @@ export default function Square({ pos, value, handleUpdateSquare }: Props) {
         outline: '1px solid',
       }}
       onClick={() => {
-        handleUpdateSquare(pos);
+        handleUpdateSquare(node.coord);
       }}
     >
-      {value}
+      {node.type > NodeType.End && [
+        <p
+          key={0}
+          style={{
+            position: 'absolute',
+            fontSize: '0.5rem',
+            top: '-0.5rem',
+            left: '0.15rem',
+          }}
+        >
+          {node.gCost.toFixed(1)}
+        </p>,
+        <p key={1}>{node.fCost.toFixed(1)}</p>,
+        <p
+          key={2}
+          style={{
+            position: 'absolute',
+            fontSize: '0.5rem',
+            bottom: '-0.5rem',
+            right: '0.15rem',
+          }}
+        >
+          {node.hCost.toFixed(1)}
+        </p>,
+      ]}
     </div>
   );
+}
+
+export class Node {
+  x: number;
+  y: number;
+  coord: string;
+  fCost: number;
+  hCost: number;
+  gCost: number;
+  type: NodeType;
+  neighbours: [string, number][];
+  prevNode: Node | undefined;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    this.coord = `${x},${y}`;
+    this.type = NodeType.Empty;
+    this.fCost = Number.MAX_VALUE;
+    this.hCost = 0;
+    this.gCost = Number.MAX_VALUE;
+    this.neighbours = Node.getNeighbours(x, y);
+  }
+
+  calculateFCost() {
+    this.fCost = this.hCost + this.gCost;
+  }
+
+  calculateHCost([x, y]: [number, number]) {
+    this.hCost = Math.abs(this.x - x) + Math.abs(this.y - y);
+  }
+
+  static getNeighbours(x: number, y: number): [string, number][] {
+    let neighbours: [string, number][] = [];
+
+    if (x > 0) {
+      neighbours.push([`${x - 1},${y}`, 1]);
+
+      if (y > 0) {
+        neighbours.push([`${x - 1},${y - 1}`, 1.4]);
+      }
+
+      if (y < SIZE - 1) {
+        neighbours.push([`${x - 1},${y + 1}`, 1.4]);
+      }
+    }
+
+    if (x < SIZE - 1) {
+      neighbours.push([`${x + 1},${y}`, 1]);
+
+      if (y > 0) {
+        neighbours.push([`${x + 1},${y - 1}`, 1.4]);
+      }
+
+      if (y < SIZE - 1) {
+        neighbours.push([`${x + 1},${y + 1}`, 1.4]);
+      }
+    }
+
+    if (y > 0) {
+      neighbours.push([`${x},${y - 1}`, 1]);
+    }
+
+    if (y < SIZE - 1) {
+      neighbours.push([`${x},${y + 1}`, 1]);
+    }
+
+    return neighbours;
+  }
 }
